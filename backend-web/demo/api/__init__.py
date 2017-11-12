60bff1e89dfae31ea7e1ec8401dc2c40dab1c85b
XiangChen <wasdnsxchen@gmail.com>
2017-11-12 23:01:15 +0800

    Update python backend (#39)

--- backend-web/demo/api/__init__.py
+++ backend-web/demo/api/__init__.py
@@ -3,3 +3,4 @@ from flask_restful import Api
 api = Api()
 
 from . import manager, activity, department
+from models import db
--- backend-web/demo/api/department.py
+++ backend-web/demo/api/department.py
@@ -3,6 +3,10 @@ from . import api
 from models.models import Department, ChildDepartment
 from sqlalchemy import desc
 
+UNDER_AUDIT = 1
+APPROVED_AUDIT = 2
+REJECTED_AUDIT = 3
+
 department_fields = {
     'department_id': fields.Integer,
     'activity_num': fields.Integer,
@@ -14,6 +18,7 @@ department_fields = {
     'image_url': fields.String,
     'member_num': fields.Integer,
     'emergency_phone': fields.String,
+    'state': fields.Integer,
 }
 
 child_department_fields = {
@@ -35,12 +40,14 @@ class DepartmentDetails(Resource):
 class DepartmentResource(Resource):
     @marshal_with(department_fields)
     def get(self):
-        page = int(request.args.get("page"))
-        print page
+        page = request.args.get("page")
+        if page == None:
+            page = 1
+        page = int(page)
         if page <= 0:
             abort(404)
-        pagination = Department.query.order_by(Department.department_id).paginate(page, per_page=10, error_out=True)
-        return pagination
+        pagination = Department.query.filter_by(state=APPROVED_AUDIT).order_by(Department.department_id).paginate(page, per_page=10, error_out=True)
+        return pagination.items
 
 class ChildDepartmentResource(Resource):
     @marshal_with(child_department_fields)
@@ -50,9 +57,39 @@ class ChildDepartmentResource(Resource):
         child_department = ChildDepartment.query.filter_by(department_id=department_id).all()
         return child_department
 
-#class AuditDepartment(Resource):
+class AuditDepartment(Resource):
 
+    @marshal_with(department_fields)
+    def get(self):
+        page = request.args.get("page")
+        if page == None:
+            page = 1
+        page = int(page)
+        if page <= 0:
+            abort(404)
+        pagination = Department.query.filter_by(state=UNDER_AUDIT).order_by(Department.department_id).paginate(page, per_page=10, error_out=True)
+        return pagination.items
+
+    @marshal_with(department_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('department_id',type=int)
+        parser.add_argument('state',type=int)
+        args = parser.parse_args() 
+        #return args
+        if args.department_id == None:
+            abort(404)
+        if args.state == APPROVED_AUDIT or args.state == REJECTED_AUDIT:
+            department = Department.query.filter_by(department_id=args.department_id, state=UNDER_AUDIT).first()
+            if department == None:
+                abort(404)
+            department.state = args.state
+            db.session.add(department)
+            db.session.commit()
+        else : 
+            abort(404)
 
 api.add_resource(DepartmentResource, '/department')
 api.add_resource(DepartmentDetails, '/department/<int:department_id>')
 api.add_resource(ChildDepartmentResource, '/department/<int:department_id>/child')
+api.add_resource(AuditDepartment, '/department/audit')
